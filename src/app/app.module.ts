e1eb16cd6d457f150a95a1a44cb4b87d3c4af065
salmarakha <61853942+salmarakha@users.noreply.github.com>
2022-06-07 21:36:41 +0200

    fix folder naming typo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './compnonents/header/header.component';
-import { ListBlogsComponent } from './compnonents/list-blogs/list-blogs.component';
-import { AddBlogComponent } from './compnonents/add-blog/add-blog.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ListBlogsComponent } from './components/list-blogs/list-blogs.component';
+import { AddBlogComponent } from './components/add-blog/add-blog.component';
+import { LikesComponent } from './components/likes/likes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ListBlogsComponent,
-    AddBlogComponent
+    AddBlogComponent,
+    LikesComponent
   ],
   imports: [
     BrowserModule,
